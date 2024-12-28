0045e4a7c4e3d9f8fb29e7986200e78b1f4081c6
Титов Роман <rt-webdesign@mail.ru>
2024-12-28 11:26:19 +0100

    Move wheel even handler to read list

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component, Inject, isDevMode, OnInit } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ListComponent } from '@/components/list/list.component';
 import { MessageComponent } from './components/message/message.component';
 import { StateService } from './services/state.service';
-import { PopupService } from './services/popup.service';
 import { SettingsComponent } from '@/components/settings/settings.component';
-import { EntriesProviderService } from '@/services/entries-provider.service';
 import { HeadComponent } from '@/components/head/head.component';
-import { ReadService } from '@/services/read.service';
 
 @Component({
   selector: 'app-root',
@@ -22,80 +19,11 @@ export class AppComponent implements OnInit {
   public constructor(
     private titleService: Title,
     private state: StateService,
-    private readService: ReadService,
-    private popupService: PopupService,
-    private entriesProviderService: EntriesProviderService,
-    @Inject('isGetEntriesInTestMode') private isGetEntriesInTestMode: boolean,
   ) {}
 
   ngOnInit(): void {
     this.title += isDevMode() ? ' - DEVELOPMENT' : '';
     this.titleService.setTitle(this.title);
-
-    window.addEventListener('keydown', this.handlerKeyDown.bind(this));
-
-    const handleWheel = this.handleWheel.bind(this);
-    this.popupService.isOpen$.subscribe((value) => {
-      if (value) {
-        window.addEventListener('wheel', handleWheel);
-      } else {
-        window.removeEventListener('wheel', handleWheel);
-      }
-    });
-
-    if (this.isGetEntriesInTestMode) {
-      const entries = this.entriesProviderService.getTestEntries();
-      this.readService.entries = entries;
-      this.readService.currentEntry = entries[0];
-      this.readService.currentEntryIndex = 0;
-    }
-
     this.state.getSections();
-  }
-
-  handlerKeyDown(event: KeyboardEvent) {
-    const target = event.target as HTMLElement;
-    if (target.closest('form')) {
-      return;
-    }
-
-    if (['ArrowLeft', 'ArrowUp'].includes(event.code)) {
-      this.readService.goPrevious();
-      event.preventDefault();
-    }
-
-    if (['ArrowRight', 'ArrowDown'].includes(event.code)) {
-      this.readService.goNext();
-      event.preventDefault();
-    }
-
-    if (event.code === 'Space') {
-      this.readService.translate();
-      event.preventDefault();
-    }
-
-    if (event.code === 'Enter') {
-      if (event.shiftKey) {
-        this.popupService.isOpen$.next(true);
-      } else if (event.ctrlKey) {
-        this.readService.translate();
-      } else {
-        this.readService.goNext();
-      }
-      event.preventDefault();
-    }
-
-    if (event.code === 'Escape') {
-      this.popupService.isOpen$.next(false);
-      event.preventDefault();
-    }
-  }
-
-  handleWheel(event: WheelEvent) {
-    if (event.deltaY < 0) {
-      this.readService.goPrevious();
-    } else {
-      this.readService.goNext();
-    }
   }
 }
--- frontend/src/app/components/list/read/list-read.component.ts
+++ frontend/src/app/components/list/read/list-read.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { Entry } from '@/Entry';
 import { ItemComponent } from './item/item.component';
 import { ControlsComponent } from './controls/controls.component';
 import { PopupReadComponent } from '@/components/list/read/popup/popup.component';
 import { ReadService } from '@/services/read.service';
+import { PopupService } from '@/services/popup.service';
+import { EntriesProviderService } from '@/services/entries-provider.service';
 
 @Component({
   selector: 'list-read',
@@ -13,7 +15,14 @@ import { ReadService } from '@/services/read.service';
   styles: ':host{display: flex;flex-direction: column;flex-grow: 1;}',
 })
 export class ListReadComponent {
-  constructor(private readService: ReadService) {}
+  readService: ReadService = inject(ReadService);
+  popupService: PopupService = inject(PopupService);
+  entriesService: EntriesProviderService = inject(EntriesProviderService);
+
+  constructor(
+    private entriesProviderService: EntriesProviderService,
+    @Inject('isGetEntriesInTestMode') private isGetEntriesInTestMode: boolean,
+  ) {}
 
   get entries(): Entry[] {
     return this.readService.entries;
@@ -21,5 +30,76 @@ export class ListReadComponent {
 
   get isEntriesLoaded(): boolean {
     return !!this.entries.length;
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('keydown', this.handlerKeyDown.bind(this));
+
+    const handleWheel = this.handleWheel.bind(this);
+    this.popupService.isOpen$.subscribe((value) => {
+      if (value) {
+        window.addEventListener('wheel', handleWheel);
+      } else {
+        window.removeEventListener('wheel', handleWheel);
+      }
+    });
+
+    if (this.isGetEntriesInTestMode) {
+      const entries = this.entriesService.getTestEntries();
+      this.readService.entries = entries;
+      this.readService.currentEntry = entries[0];
+      this.readService.currentEntryIndex = 0;
+    }
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('keydown', this.handlerKeyDown.bind(this));
+    window.removeEventListener('wheel', this.handleWheel.bind(this));
+  }
+
+  handlerKeyDown(event: KeyboardEvent) {
+    const target = event.target as HTMLElement;
+    if (target.closest('form')) {
+      return;
+    }
+
+    if (['ArrowLeft', 'ArrowUp'].includes(event.code)) {
+      this.readService.goPrevious();
+      event.preventDefault();
+    }
+
+    if (['ArrowRight', 'ArrowDown'].includes(event.code)) {
+      this.readService.goNext();
+      event.preventDefault();
+    }
+
+    if (event.code === 'Space') {
+      this.readService.translate();
+      event.preventDefault();
+    }
+
+    if (event.code === 'Enter') {
+      if (event.shiftKey) {
+        this.popupService.isOpen$.next(true);
+      } else if (event.ctrlKey) {
+        this.readService.translate();
+      } else {
+        this.readService.goNext();
+      }
+      event.preventDefault();
+    }
+
+    if (event.code === 'Escape') {
+      this.popupService.isOpen$.next(false);
+      event.preventDefault();
+    }
+  }
+
+  handleWheel(event: WheelEvent) {
+    if (event.deltaY < 0) {
+      this.readService.goPrevious();
+    } else {
+      this.readService.goNext();
+    }
   }
 }
