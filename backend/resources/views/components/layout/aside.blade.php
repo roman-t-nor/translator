e0571f60b4e425b2b21b73f1639e27f945a97554
Титов Роман <rt-webdesign@mail.ru>
2025-01-01 17:58:29 +0100

    Backend: aside links (top)

--- backend/resources/views/components/layout/aside.blade.php
+++ backend/resources/views/components/layout/aside.blade.php
@@ -1,9 +1,11 @@
 <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
     <div class="sidebar-brand">
-        <a href="/admin/" class="brand-link">
+        <div class="brand-link">
             <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow">
-            <span class="brand-text fw-light">Translator | Admin</span>
-        </a>
+            <span class="brand-text fw-light">
+                <a href="/" target="_blank">Translator</a> | <a href="/admin">Admin</a>
+            </span>
+        </div>
     </div>
     <div class="sidebar-wrapper">
         <x-navigation/>
