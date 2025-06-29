550c8948e46623e9ce8625c4e78d9b436df0154f
Roman Tytov <roman.t.nor@gmail.com>
2025-06-29 12:10:03 +0200

    blade

--- backend/resources/views/components/layout/aside.blade.php
+++ backend/resources/views/components/layout/aside.blade.php
@@ -3,7 +3,7 @@
         <div class="brand-link">
             <img src="/build/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow">
             <span class="brand-text fw-light">
-                <a href="/" target="_blank">Translator</a> | <a href="/admin">Admin</a>
+                <a href="/" target="_blank">Translator</a> | <a href="/admin">Admin!!!</a>
             </span>
         </div>
     </div>
